5a3862797021d651df8ccb4f71edd853c0a92be3
amyrneto <amyrneto@gmail.com>
2019-09-11 13:08:51 -0300

    Initial Commit

--- dataShareDLL/dataShareDLL.cpp
+++ dataShareDLL/dataShareDLL.cpp
@@ -3,4 +3,24 @@
 
 #include "stdafx.h"
 
+#include "dlltest.h"
 
+#pragma data_seg("SHARED")
+int x = 0;
+#pragma data_seg()
+
+extern "C" __declspec(dllexport)
+
+int get()
+{
+	return x;
+}
+
+extern "C" __declspec(dllexport)
+
+void set(int temp)
+{
+	x = temp;
+}
+
+#pragma comment(linker, "/section:SHARED,RWS")  
