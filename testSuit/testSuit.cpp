992fb94c25ef103eeca545aa38f9c84b91bf9719
amyrneto <amyrneto@gmail.com>
2019-09-11 13:30:09 -0300

    Added a testSuit console application for test purposes.

---
+++ testSuit/testSuit.cpp
@@ -0,0 +1,10 @@
+// testSuit.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
